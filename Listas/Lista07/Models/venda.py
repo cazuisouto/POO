961aae33032242382d2edc30e4746b4f38652bdc
Cazuí Souto <116229302+cazuisouto@users.noreply.github.com>
2026-05-16 15:10:14 +0000

    Class Venda e VendaDAO

--- Listas/Lista07/Models/venda.py
+++ Listas/Lista07/Models/venda.py
@@ -21,13 +21,18 @@ class Venda:
         else: raise ValueError("ID deve ser um número inteiro positivo.")
 
     def set_data(self, data):
-        if ...:
-            ... #necessário validar o formato da data (ex: "dd/mm/aaaa")
-        else:
-            ...
+        try:
+            # converte a string para datetime
+            data_convertida = datetime.strptime(data, '%d/%m/%Y')
+
+            # salva no atributo
+            self.__data = data_convertida         
+
+        except ValueError:
+            print("Data inválida! Use dd/mm/aaaa")
 
     def set_carrinho(self, carrinho):
-        if carrinho == bool:
+        if carrinho == bool(carrinho):
             self.__carrinho = carrinho
         else: raise ValueError("Carrinho deve ser do tipo booleano.")
 
@@ -41,5 +46,78 @@ class Venda:
             self.__id_cliente = id_cliente
         else: raise ValueError("ID do cliente deve ser um número inteiro positivo.")
 
-    #falta o método __str__ e to_json
-    #falta a classe VendaDAO para persistência de dados
+    def __str__(self):
+        return f"ID: {self.get_id()} - Data: {self.get_data().strftime('%d/%m/%Y')} - Carrinho: {self.get_carrinho()} - Total: R${self.get_total():.2f} - ID Cliente: {self.get_id_cliente()}"
+    
+    def to_json(self):
+        return json.dumps({
+            "id": self.get_id(),
+            "data": self.get_data().strftime('%d/%m/%Y'),
+            "carrinho": self.get_carrinho(),
+            "total": self.get_total(),
+            "id_cliente": self.get_id_cliente()
+        },ensure_ascii=False)
+
+
+class VendaDAO: #persistência de dados
+    objetos = []
+
+    @classmethod
+    def abrir(cls):
+        cls.objetos = []
+        try:
+            with open("vendas.json", "r") as arquivo:
+                data = json.load(arquivo)
+                for obj in data:
+                    v = Venda(obj['id'], obj['data'], obj['carrinho'], obj['total'], obj['id_cliente'])
+                    cls.objetos.append(v)
+        except FileNotFoundError:
+            return cls.objetos
+
+    @classmethod    
+    def salvar(cls):
+        with open("vendas.json", "w") as arquivo:
+            json.dump(cls.objetos, arquivo, default=Venda.to_json)
+
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        if len(cls.objetos) == 0:
+            id = 1
+        else:
+            id = max(cls.objetos, key = lambda x : x.get_id()).get_id() + 1
+
+        obj.set_id(id)
+        cls.objetos.append(obj)
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.objetos  
+    
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        for obj in cls.objetos:
+            if obj.get_id() == id:
+                return obj
+        return None
+
+    @classmethod
+    def atualizar(cls, obj):
+        x = cls.listar_id(obj.get_id())
+        if x != None:
+            cls.set_data(obj.get_data())
+            cls.set_carrinho(obj.get_carrinho())
+            cls.set_total(obj.get_total())
+            cls.set_id_cliente(obj.get_id_cliente())   
+
+    @classmethod
+    def excluir(cls, obj):
+        x = cls.listar_id(obj)
+        if x != None:
+            cls.objetos.remove(x)
+            cls.salvar()
+        else:
+            raise ValueError("Cliente não encontrado.")
+        
