a151f3eb43cc6ab32c3930188bb9dd83309a0567
Cazuí Souto <116229302+cazuisouto@users.noreply.github.com>
2026-05-16 03:04:49 +0000

    Import classes prontas do models para uso

---
+++ Listas/Lista07/Views/View.py
@@ -0,0 +1,4 @@
+from Models.cliente import Cliente, ClienteDAO
+from Models.produto import Produto, ProdutoDAO
+from Models.categoria import Categoria, CategoriaDAO
+from Models.favoritos import Favoritos, FavoritosDAO
