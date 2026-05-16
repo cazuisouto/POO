97f38030f6a20eefe8f3694eaaa9f767ad042296
Cazuí Souto <116229302+cazuisouto@users.noreply.github.com>
2026-05-16 02:51:10 +0000

    Class Produto OK e Class ProdutoDAO falta terminar

---
+++ Listas/Lista07/Models/produto.py
@@ -0,0 +1,74 @@
+import json
+
+class Produto:
+    def __init__(self, id, descricao, preco, estoque, id_categoria):
+        self.set_id(id)
+        self.set_descricao(descricao)
+        self.set_preco(preco)
+        self.set_estoque(estoque)
+        self.set_id_categoria(id_categoria)
+
+    def get_id(self):        return self.__id
+    def get_descricao(self): return self.__descricao
+    def get_preco(self):     return self.__preco
+    def get_estoque(self):   return self.__estoque
+    def get_id_categoria(self): return self.__id_categoria
+
+    def set_id(self, id):
+        if (id >0) and (id == int(id)):
+            self.__id = id
+        else: raise ValueError("ID deve ser um número inteiro positivo.")
+
+    def set_descricao(self, descricao):
+        if len(descricao) > 0:
+            self.__descricao = descricao
+        else: raise ValueError("Descrição deve conter pelo menos um caractere.")
+
+    def set_preco(self, preco):
+        if preco >= 0:
+            self.__preco = preco
+        else: raise ValueError("Preço deve ser um número não negativo.")
+
+    def set_estoque(self, estoque):
+        if estoque >= 0:
+            self.__estoque = estoque
+        else: raise ValueError("Estoque deve ser um número não negativo.")
+
+    def set_id_categoria(self, id_categoria):
+        if (id_categoria >0) and (id_categoria == int(id_categoria)):
+            self.__id_categoria = id_categoria
+        else: raise ValueError("ID da categoria deve ser um número inteiro positivo.")
+    
+    def __str__(self):
+        return f"ID: {self.get_id()} - Descrição: {self.get_descricao()} - Preço: R${self.get_preco():.2f} - Estoque: {self.get_estoque()} - ID Categoria: {self.get_id_categoria()}"
+    
+    def to_json(self):
+        return json.dumps({
+            "id": self.get_id(),
+            "descricao": self.get_descricao(),
+            "preco": self.get_preco(),
+            "estoque": self.get_estoque(),
+            "id_categoria": self.get_id_categoria()
+        })
+    
+
+class ProdutoDAO: #persistência de dados
+    objetos = []
+
+    @classmethod
+    def abrir(cls):
+        objetos = []
+        try:
+            with open("produtos.json", "r") as arquivo:
+                dados = json.load(arquivo)
+                for obj in dados:
+                    p = Produto(obj['id'], obj['descricao'], obj['preco'], obj['estoque'], obj['id_categoria'])
+                    cls.objetos.append(p)
+        except FileNotFoundError:
+            cls.objetos = []
+
+    @classmethod
+    def salvar(cls):
+        with open("produtos.json", "w") as arquivo:
+            json.dump(cls.objetos,arquivo, default= Produto.to_json)
+      
